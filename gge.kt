fbdd8fe21c96fd0ba241e33b63d85802aaa8d887
Katerina Pustovalova <51330147+Luthineth@users.noreply.github.com>
2019-06-07 11:46:13 +0300

    Update gge.kt

--- gge.kt
+++ gge.kt
@@ -9,7 +9,7 @@ fun main(args: Array<String>) {
         else
         {val k=b*b-4*a*c
             val d = sqrt(k)
-            if (k.equals(0.0)) {println(k)} else{
+            if (k.equals(0.0)) {val x=-c;println(x)} else{
                 if (k<0) {
                     println("no roots")} else {
                     val x1 = (-b + d) / (2 * a)
